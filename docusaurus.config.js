d128d76958e068a2d0f05f8f0e65774f8f5677d0
xoron <email@example.com>
2024-09-15 16:53:30 +0200

    adding edit links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -112,8 +112,8 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl:
+            'https://github.com/positive-intentions/website/tree/main/',
         },
         // blog: false,
         blog: {
@@ -124,8 +124,8 @@ const config = {
 
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl:
+            'https://github.com/positive-intentions/website/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
