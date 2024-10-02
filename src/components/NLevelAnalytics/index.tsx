11c3de03600d8f1d589f19cacc83b8216bea3c7e
xoron <rmaher84@gmail.com>
2024-10-02 09:30:05 +0200

    Update index.tsx

--- src/components/NLevelAnalytics/index.tsx
+++ src/components/NLevelAnalytics/index.tsx
@@ -37,7 +37,7 @@ export const HeadScript = function () {
         // }, [])
 
     return <Head>
-            <script data-grow-initializer="">!(function(){window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZTplZWZkNDM4NC1jYjc2LTRiNzctOWViNi0wZTVkODY4MzY0Njk=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();</script>
+        <script data-grow-initializer="">{`!(function(){window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZTplZWZkNDM4NC1jYjc2LTRiNzctOWViNi0wZTVkODY4MzY0Njk=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();`}</script>
     </Head>;
 }
 
