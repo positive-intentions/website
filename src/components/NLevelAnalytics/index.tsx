fcf1acfe1c2f8453a62bd092ea83fee0c4e12b5d
xoron <rmaher84@gmail.com>
2024-10-02 09:19:15 +0200

    Update index.tsx

--- src/components/NLevelAnalytics/index.tsx
+++ src/components/NLevelAnalytics/index.tsx
@@ -21,22 +21,24 @@ export const HeadScript = function () {
         //     }
         // }
 
-        const functionToCall = `
-        !(function(){window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZTplZWZkNDM4NC1jYjc2LTRiNzctOWViNi0wZTVkODY4MzY0Njk=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();
-        `
-        const elementToInsertIntoHead = document.createElement('script');
-        elementToInsertIntoHead.innerHTML = functionToCall;
-        elementToInsertIntoHead.type = 'text/javascript';
-        elementToInsertIntoHead.async = true;
-        elementToInsertIntoHead.setAttribute('data-grow-initializer', '');
+        // const functionToCall = `
+        // !(function(){window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZTplZWZkNDM4NC1jYjc2LTRiNzctOWViNi0wZTVkODY4MzY0Njk=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();
+        // `
+        // const elementToInsertIntoHead = document.createElement('script');
+        // elementToInsertIntoHead.innerHTML = functionToCall;
+        // elementToInsertIntoHead.type = 'text/javascript';
+        // elementToInsertIntoHead.async = true;
+        // elementToInsertIntoHead.setAttribute('data-grow-initializer', '');
     
-        const firstScript = document.getElementsByTagName('script')[0];
-        if (firstScript && firstScript.parentNode) {
-            firstScript.parentNode.insertBefore(elementToInsertIntoHead, firstScript);
-        }
-        }, [])
+        // const firstScript = document.getElementsByTagName('script')[0];
+        // if (firstScript && firstScript.parentNode) {
+        //     firstScript.parentNode.insertBefore(elementToInsertIntoHead, firstScript);
+        // }
+        // }, [])
 
-    return <Head></Head>;
+    return <Head>
+            <script data-grow-initializer="">!(function(){window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZTplZWZkNDM4NC1jYjc2LTRiNzctOWViNi0wZTVkODY4MzY0Njk=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();</script>
+    </Head>;
 }
 
 export default HeadScript;
